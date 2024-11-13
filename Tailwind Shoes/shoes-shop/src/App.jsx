6ee611d98378556045005cc1cc0f78e33bf6101a
arun sha <>
2024-11-13 14:26:06 +0530

    Sample shoes shop init application

---
+++ Tailwind Shoes/shoes-shop/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div className="flex items-center justify-center h-screen text-2xl">This is my first Vite and Tailwind css project</div>;
+}
